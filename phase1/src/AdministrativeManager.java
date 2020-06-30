73050494b9e7c73826bed524b5994221a457da93
Lingyun Wang <lucylingyun.wang@mail.utoronto.ca>
2020-06-30 00:29:25 -0400

    phase 1 Lucy draft

---
+++ phase1/src/AdministrativeManager.java
@@ -0,0 +1,13 @@
+package src;
+
+public class AdministrativeManager {
+
+    private AdministrativeUser[] administrativeUsers;
+    //private Notification[] notifications = [];
+    private int transactionLimit;
+    private int lendBeforeBorrow = 1;
+
+    AdministrativeManager(AdministrativeUser[] administrativeUsers){
+
+    }
+}

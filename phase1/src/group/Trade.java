bb8b995ff40c4c2811a591e4c5fe881d79c6834a
Grace Leung <greyse.leung@mail.utoronto.ca>
2020-07-08 21:08:32 -0400

    Changed date and time to a timestamp

--- phase1/src/group/Trade.java
+++ phase1/src/group/Trade.java
@@ -1,10 +1,12 @@
 package group;
 
+import java.sql.Timestamp;
+
 public class Trade{
     // Trading Details
     private final int tradeID;
-    private final User user1;
-    private final User user2;
+    private final PersonalUser user1;
+    private final PersonalUser user2;
     private int user1Edits;
     private int user2Edits;
     private boolean user1Confirms;
@@ -17,27 +19,25 @@ public class Trade{
     private boolean isClosed;
 
     // Meeting Details
-    private String date; // formatted year-month-day
-    private String time; // formatted hours: minutes
+    private Timestamp dateAndTime;
     private String location;
 
     // If either item1 or item2 is null then it is a one-way trade or else it is a two-way trade
-    public Trade(int tradeID, User user1, User user2, Item item1, Item item2, boolean isPermanent, String date,
-                 String time, String location){
+    public Trade(int tradeID, PersonalUser user1, PersonalUser user2, Item item1, Item item2, boolean isPermanent,
+            Timestamp dateAndTime, String location){
         this.tradeID = tradeID;
         this.user1 = user1;
         this.user2 = user2;
         this.item1 = item1;
         this.item2 = item2;
         this.isPermanent = isPermanent;
-        this.date = date;
-        this.time = time;
+        this.dateAndTime = dateAndTime;
         this.location = location;
 
         // Default Values
         this.user1Edits = 0;
         this.user2Edits = 0;
-        this.user1Confirms = false;
+        this.user1Confirms = true;
         this.user2Confirms = false;
         this.isClosed = true;
     }
@@ -54,11 +54,10 @@ public class Trade{
     public Item getItem2(){ return item2;}
     public boolean getIsPermanent(){ return isPermanent;}
     public boolean getIsClosed(){ return isClosed;}
-    public String getDate(){ return date;}
-    public String getTime(){ return time;}
+    public Timestamp getDateAndTime(){ return dateAndTime;}
     public String getLocation(){ return location;}
 
-    // Setters -- Do we allow changing items or permanency of trade
+    // Setters
     public void increaseUser1Edits(){ user1Edits++;}
     public void increaseUser2Edits(){ user2Edits++;}
     public void confirmUser1(){ user1Confirms = true;}
@@ -67,8 +66,7 @@ public class Trade{
     public void unconfirmUser2(){ user2Confirms = false;}
     public void openTrade(){ isClosed = false;}
     public void closeTrade(){ isClosed = true;}
-    public void setDate(String new_date){ date = new_date;}
-    public void setTime(String new_time){ time = new_time;}
+    public void setDate(Timestamp new_dateAndTime){ dateAndTime = new_dateAndTime;}
     public void setLocation(String new_location){ location = new_location;}
 
     /**
@@ -79,14 +77,14 @@ public class Trade{
         if (item1 == null && item2 == null){
             return "This is an invalid trade.";
         } else if (item1 == null){
-            return user1.getName() + " lends to " + user2.getName() + " at " + this.location + " on " + this.date + ", "
-                    + this.time;
+            return user1.getName() + " lends to " + user2.getName() + " at " + this.location + " on " +
+                    this.dateAndTime;
         } else if (item2 == null){
-            return user1.getName() + " borrows from " + user2.getName() + " at " + this.location + " on " + this.date +
-                    ", " + this.time;
+            return user1.getName() + " borrows from " + user2.getName() + " at " + this.location + " on " +
+                    this.dateAndTime;
         } else {
-            return user1.getName() + " trades with " + user2.getName() + " at " + this.location + " on " + this.date +
-                    ", " + this.time;
+            return user1.getName() + " trades with " + user2.getName() + " at " + this.location + " on " +
+                    this.dateAndTime;
         }
     }
 }

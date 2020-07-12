d5daf443be0a210a5c1703202b37d830123c37d1
Dan Lyu <dan@muddy.ca>
2020-07-12 15:51:47 -0400

    Add an anonymous inner class example for Filter in PersonalUserManager

--- phase1/src/group/user/PersonalUserManager.java
+++ phase1/src/group/user/PersonalUserManager.java
@@ -1,5 +1,6 @@
 package group.user;
 
+import group.repository.Filter;
 import group.repository.Repository;
 
 import java.util.ArrayList;
@@ -18,7 +19,13 @@ public class PersonalUserManager {
 
     public void exampleOfFilter() {
         Iterator<PersonalUser> usersToBeFrozen = personalUserRepository.iterator(PersonalUser::getShouldBeFreezed);
-        Iterator<PersonalUser> usersToBeFrozen2 = personalUserRepository.iterator(personalUser -> personalUser.getLendCount() < personalUser.getBorrowCount()); // they are the same
+        Iterator<PersonalUser> usersToBeFrozen2 = personalUserRepository.iterator(personalUser -> personalUser.getLendCount() < personalUser.getBorrowCount());
+        Iterator<PersonalUser> usersToBeFrozen3 = personalUserRepository.iterator(new Filter<PersonalUser>() {
+            @Override
+            public boolean match(PersonalUser personalUser) {
+                return personalUser.getLendCount() < personalUser.getBorrowCount();
+            }
+        }); // usersToBeFrozen / usersToBeFrozen2 / usersToBeFrozen3 work the same here
         // this iterator has all PersonalUsers that need to be frozen
     }
 

79e220460e4c5b5ecf685032ed1a75e076271fa2
Grace Leung <greyse.leung@mail.utoronto.ca>
2020-07-14 03:42:44 -0400

    Improve date validator to be after current time (can be adjusted to be after one day later).

--- phase1/src/group/menu/validator/DateValidator.java
+++ phase1/src/group/menu/validator/DateValidator.java
@@ -1,19 +1,24 @@
 package group.menu.validator;
 
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+
 public class DateValidator implements Validator {
 
     @Override
     public boolean validate(String input) {
         String[] data = input.split("-");
         if (data.length == 5){
-            for(String s: data) {
-                try {
-                    Integer.parseInt(s);
-                } catch (NumberFormatException e) { // is this allowed
-                    return false;
-                }
+            try {
+                int year = Integer.parseInt(data[0]);
+                int month = Integer.parseInt(data[1]);
+                int day = Integer.parseInt(data[2]);
+                int hour = Integer.parseInt(data[3]);
+                int minute = Integer.parseInt(data[4]);
+                return (LocalDateTime.of(year, month, day, hour, minute)).isAfter(LocalDateTime.now());
+            } catch (NumberFormatException | DateTimeException e) { // is this allowed
+                return false;
             }
-            return true;
         }
         return false;
     }

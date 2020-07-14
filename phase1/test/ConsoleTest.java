c6ab64028e4b2d69e1f22fe0d410cb16a57263e7
Dan Lyu <dan@muddy.ca>
2020-07-14 03:53:08 -0400

    Test GeneralValidator and PasswordEncryption

--- phase1/test/ConsoleTest.java
+++ phase1/test/ConsoleTest.java
@@ -1,4 +1,6 @@
 import group.menu.processor.CSVInjectionPrevention;
+import group.menu.processor.PasswordEncryption;
+import group.menu.validator.GeneralValidator;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,9 +14,28 @@ public class ConsoleTest {
         assertEquals(csvInjectionPrevention.process(test1), "a23");
 
         String test2 = "a2";
+    }
 
-        assertTrue(test1.matches(".*[;,].*"));
-        assertFalse(test2.matches(".*[;,].*"));
+    @Test
+    public void generalValidatorTest() {
+        GeneralValidator generalValidator = new GeneralValidator(GeneralValidator.InputType.Number, 5, 10, true);
+        assertFalse(generalValidator.validate("1232344;,"));
+        assertTrue(generalValidator.validate("1232344"));
+        assertTrue(generalValidator.validate("1232344.2"));
+        assertFalse(generalValidator.validate("123="));
+        assertFalse(generalValidator.validate("123"));
+
+        GeneralValidator generalValidator2 = new GeneralValidator(GeneralValidator.InputType.String, 3, 10, true);
+        assertTrue(generalValidator2.validate("mmm"));
+    }
+
+    @Test
+    public void PasswordEncryptionTest() {
+        PasswordEncryption passwordEncryption = new PasswordEncryption();
+        String password = "pikapika";
+        String encrypted = passwordEncryption.process(password);
+        assertEquals(passwordEncryption.process("pikapika"), encrypted);
+        assertNotEquals(passwordEncryption.process("pikapika2"), encrypted);
     }
 
 

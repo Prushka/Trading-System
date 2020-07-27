7475fce77bb028cadde150ebb95e30b64ecbd19f
leunggr9 <greyse.leung@mail.utoronto.ca>
2020-07-27 15:10:40 -0400

    ....

--- phase2/app/src/main/java/com/phase2/trade/trade/Trade.java
+++ phase2/app/src/main/java/com/phase2/trade/trade/Trade.java
@@ -8,61 +8,43 @@ import java.time.LocalDateTime;
 
 class Trade extends MappableBase implements CSVMappable, UniqueId {
     // Trading Details
-    private Integer tradeID;
-    private Integer user1;
-    private Integer user2;
-    private Integer user1Edits;
-    private Integer user2Edits;
-    private Boolean user1Confirms;
-    private Boolean user2Confirms;
-    private Integer prevMeeting;
-
-    // Item Details
-    private Integer item1;
-    private Integer item2;
-    private Boolean isPermanent;
-    private Boolean isClosed;
+    private List<Integer> users;
+    private List<Boolean> confirmations;
+    private List<Integer> edits;
+    private List<List<Integer>> items;
 
     // Meeting Details
+    private Integer tradeID;
     private LocalDateTime dateAndTime;
     private String location;
+    private Boolean isClosed;
 
     /**
      * @param record Representation of a trade's parameters
      */
-    // Needed to implement Mappable Base
     public Trade(List<String> record){
         super(record);
     }
 
     /**
-     * Describes a one-way or two-way trade
-     * @param user1 The user ID of the trade initiator
-     * @param user2 The user ID of the trade respondent
-     * @param item1 The item ID of the item that the initiator wishes to lend
-     * @param item2 The item ID of the item that the initiator wishes to borrow
-     * @param isPermanent True iff this trade is permanent
-     * @param dateAndTime When this trade occurs
-     * @param location Where this trade occurs
-     * @param prevMeeting The previous trade meeting
+     * @param users All the userID's associated with this trade
+     * @param items All the items associated with this trade. Each list corresponds to the desired
+     *              items of the userID in users with the same index
+     * @param dateAndTime When this trade takes place
+     * @param location Where this trade takes place
      */
-    public Trade(Integer user1, Integer user2, Integer item1, Integer item2, Boolean isPermanent, LocalDateTime dateAndTime,
-                 String location, Integer prevMeeting){
-        this.user1 =  user1;
-        this.user2 =  user2;
-        this.item1 =  item1;
-        this.item2 =  item2;
-        this.isPermanent = isPermanent;
+    Trade(List<Integer> users, List<List<Integer>> items, LocalDateTime dateAndTime, String
+            location){
+        this.users = users;
+        this.items = items;
         this.dateAndTime = dateAndTime;
         this.location =  location;
-        this.prevMeeting = prevMeeting;
 
         // Default Values
-        this.user1Edits = 0;
-        this.user2Edits = 0;
-        this.user1Confirms = true;
-        this.user2Confirms = false;
-        this.isClosed = true;
+        edits = new ArrayList<>(users.size());
+        confirmations = new ArrayList<>(users.size());
+        confirmations.set(0, true);
+        isClosed = true;
     }
 
     // GETTERS
@@ -74,49 +56,19 @@ class Trade extends MappableBase implements CSVMappable, UniqueId {
     public int getUid() { return this.tradeID;}
 
     /**
-     * @return The user ID of the initiator
+     * @return The amount of times the user has edited
      */
-    public int getUser1(){ return user1;}
+    public int getUserEdits(int userID){ return this.edits.get(this.users.indexOf(userID));}
 
     /**
-     * @return The user ID of the respondent
+     * @return True iff the user has confirmed to the opening/ completion of this trade
      */
-    public int getUser2(){ return user2;}
+    public boolean getUserConfirms(int userID){ return this.confirmations.get(this.users.indexOf(userID));}
 
     /**
-     * @return The amount of times the initiator has edited
+     * @return The list of desired items for the user
      */
-    public int getUser1Edits(){ return user1Edits;}
-
-    /**
-     * @return The amount of times the respondent has edited
-     */
-    public int getUser2Edits(){ return user2Edits;}
-
-    /**
-     * @return True iff the initiator has confirmed to the opening/ completion of this trade
-     */
-    public boolean getUser1Confirms(){ return user1Confirms;}
-
-    /**
-     * @return True iff the respondent has confirmed to the opening/ completion of this trade
-     */
-    public boolean getUser2Confirms(){ return user2Confirms;}
-
-    /**
-     * @return The item ID of what the initiator wants to lend to the respondent
-     */
-    public Integer getItem1(){ return item1;}
-
-    /**
-     * @return The item ID of what the initiator wants to borrow from the respondent
-     */
-    public Integer getItem2(){ return item2;}
-
-    /**
-     * @return True iff this trade is permanent
-     */
-    public boolean getIsPermanent(){ return isPermanent;}
+    public List<Integer> getItems(int userID){ return this.items.get(this.users.indexOf(userID));}
 
     /**
      * @return True iff this trade is closed
@@ -133,11 +85,6 @@ class Trade extends MappableBase implements CSVMappable, UniqueId {
      */
     public String getLocation(){ return location;}
 
-    /**
-     * @return The trade ID of the previous trade if applicable
-     */
-    public Integer getPrevMeeting(){ return prevMeeting;}
-
     // SETTERS
 
     /**
@@ -147,34 +94,20 @@ class Trade extends MappableBase implements CSVMappable, UniqueId {
     public void setUid(int new_uid) { this.tradeID = new_uid;}
 
     /**
-     * Increases the initiator's edits by one.
+     * Increases the user's edits by one.
      */
-    public void increaseUser1Edits(){ user1Edits++;}
+    public void increaseUserEdits(int userID){ this.edits.set(this.users.indexOf(userID),
+            this.edits.get(this.users.indexOf(userID)) + 1);}
 
     /**
-     * Increase the initiator's edits by one.
+     * Confirms the user's commitment to the trade/ verification of completion
      */
-    public void increaseUser2Edits(){ user2Edits++;}
+    public void confirmUser(int userID){ this.confirmations.set(this.users.indexOf(userID), true);}
 
     /**
-     * Confirms the initiator's commitment to the trade/ verification of completion
+     * Un-confirms the user's commitment to the trade/ verification of completion
      */
-    public void confirmUser1(){ user1Confirms = true;}
-
-    /**
-     * Confirms the respondent's commitment to the trade/ verification of completion
-     */
-    public void confirmUser2(){ user2Confirms = true;}
-
-    /**
-     * Un-confirms the initiator's commitment to the trade/ verification of completion
-     */
-    public void unconfirmUser1(){ user1Confirms = false;}
-
-    /**
-     * Un-confirms the respondent's commitment to the trade/ verification of completion
-     */
-    public void unconfirmUser2(){ user2Confirms = false;}
+    public void unconfirmUser(int userID){ this.confirmations.set(this.users.indexOf(userID), false);}
 
     /**
      * Sets the state of this trade to open
@@ -197,24 +130,4 @@ class Trade extends MappableBase implements CSVMappable, UniqueId {
      * @param newLocation The new location of this trade
      */
     public void setLocation(String newLocation){ location = newLocation;}
-
-    /**
-     * @return A description of this trade.
-     */
-    @Override
-    public String toString() {
-        if (item1 == null && item2 == null){
-            return "This is an empty trade, nothing is traded between UserID: " + user1 + " and UserID: " + user2;
-        } else if (item1 == null){
-            return "UserID: " + user1 + " borrows from UserID: " + user2 + " at " + this.location + " on " +
-                    this.dateAndTime;
-        } else if (item2 == null){
-            return "UserID: " + user1 + " lends to UserID: " + user2 + " at " + this.location + " on " +
-                    this.dateAndTime;
-        } else {
-            return "UserID: " + user1 + " trades with UserID: " + user2 + " at " + this.location + " on " +
-                    this.dateAndTime;
-        }
-    }
-    
 }

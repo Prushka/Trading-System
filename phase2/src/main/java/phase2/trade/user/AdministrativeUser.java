6fe5c752b64fb800444d6cf84b4cb34dafd9e9ee
Lingyun Wang <lucylingyun.wang@mail.utoronto.ca>
2020-08-05 21:33:17 -0400

    add isHead variable to admin

--- phase2/src/main/java/phase2/trade/user/AdministrativeUser.java
+++ phase2/src/main/java/phase2/trade/user/AdministrativeUser.java
@@ -7,12 +7,23 @@ import java.util.List;
 @Entity
 public class AdministrativeUser extends User {
 
-    public AdministrativeUser(String userName, String email, String password) {
+    private boolean isHead;
+
+    public AdministrativeUser(String userName, String email, String password, boolean isHead) {
         super(userName, email, password);
+        this.isHead = isHead;
     }
 
     public AdministrativeUser() {
 
+    }
+
+    public boolean getIsHead() {
+        return this.isHead;
+    }
+
+    public void setIsHead(boolean isHead) {
+        this.isHead = isHead;
     }
 
 }

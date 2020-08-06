eb72d07faa215a55ba1ac44fc65407fb51bb554c
Dan Lyu <d.lyu@mail.utoronto.ca>
2020-08-06 16:07:34 -0400

    Make UserPermission hold set of Permissions

--- phase2/src/main/java/phase2/trade/user/UserPermission.java
+++ phase2/src/main/java/phase2/trade/user/UserPermission.java
@@ -1,37 +1,51 @@
 package phase2.trade.user;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
-//@Entity
+@Entity
 @Embeddable
 public class UserPermission {
 
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long uid;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long uid;
 
-    // @Embedded
-    // private Map<Permission, Boolean> perm;
+    @ElementCollection(targetClass=Permission.class)
+    private Set<Permission> perm;
 
-//    public Long getUid() {
-//        return uid;
-//    }
-//
-//    public void setUid(Long uid) {
-//        this.uid = uid;
-//    }
+    public UserPermission() {
+        this.perm = new HashSet<>();
+    }
 
-    // public Map<Permission, Boolean> getPerm() {
-    //     return perm;
-    // }
+    public UserPermission(Set<Permission> perm) {
+        this.perm = perm;
+    }
 
-    // public boolean hasPermission(Permission permission){
-    //     if(!perm.containsKey(permission)) return false;
-    //     return getPerm().get(permission);
-    // }
+    public Long getUid() {
+        return uid;
+    }
 
-    // public void setPerm(Map<Permission, Boolean> perm) {
-    //     this.perm = perm;
-    // }
+    public void setUid(Long uid) {
+        this.uid = uid;
+    }
+
+    public Set<Permission> getPerm() {
+        return perm;
+    }
+
+    public boolean hasPermission(Permission permission){
+        return perm.contains(permission);
+    }
+
+    public void setPerm(Set<Permission> perm) {
+        this.perm = perm;
+    }
+
+    public void removePerm(Permission perm){
+        this.perm.remove(perm);
+    }
 }

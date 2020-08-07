9248e3f51f19b33e8ec791786d70ba6ca8f4d274
Grace Leung <greyse.leung@mail.utoronto.ca>
2020-08-07 14:09:24 -0400

    adding a general layout for the trade market

--- phase2/src/main/java/phase2/trade/controller/MarketController.java
+++ phase2/src/main/java/phase2/trade/controller/MarketController.java
@@ -6,45 +6,62 @@ import com.jfoenix.controls.JFXTreeTableView;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import phase2.trade.trade.Trade;
 import phase2.trade.trade.TradeState;
+import phase2.trade.user.AccountManager;
 import phase2.trade.user.User;
 
+import javax.swing.*;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class MarketController implements Initializable {
+    GridPane grid = new GridPane();
     TableView<Trade> trades;
-    JFXTextField traders;
-    JFXTextField year;
-    JFXTextField month;
-    JFXTextField day;
-    JFXTextField hour;
-    JFXTextField minute;
-    JFXTextField country;
-    JFXTextField city;
-    JFXTextField street;
-    JFXTextField streetNum;
+    TextField traders;
+    TextField items;
+    TextField year;
+    TextField month;
+    TextField day;
+    TextField hour;
+    TextField minute;
+    TextField country;
+    TextField city;
+    TextField street;
+    TextField streetNum;
     JFXComboBox<Boolean> isPermanent;
     Button submit;
 
-    public MarketController(){
+    User currUser;
+    public MarketController(User currUser){
+        this.currUser = currUser;
         TableColumn<Trade, TradeState> statusColumn = new TableColumn<>("Trade Status: ");
         statusColumn.setMinWidth(100);
         statusColumn.setCellValueFactory(new PropertyValueFactory<>("tradeState"));
 
+        HBox dateTime = new HBox();
+        dateTime.setSpacing(10);
+
         trades = new TableView<>();
         trades.setItems(getTrades());
         trades.getColumns().setAll(statusColumn);
+
+        grid.setVgap(10); // Adds spacing veritcally
+        grid.setHgap(10); // Adds spacing horizontally
+        GridPane.setConstraints(trades, 0, 0); // adds items to the gridpane at the column and row
+        GridPane.setConstraints(dateTime, 1, 0); // adds items to the gridpane at the column and row
     }
 
     @Override
--- phase2/src/main/java/phase2/trade/controller/SideMenuController.java
+++ phase2/src/main/java/phase2/trade/controller/SideMenuController.java
@@ -49,7 +49,7 @@ public class SideMenuController extends AbstractController implements Initializa
     }
 
     private void market() {
-        Parent userPane = loadPane("market.fxml", new UserInfoPresenter(accountManager.getLoggedInUser()));
+        Parent userPane = loadPane("market.fxml", new MarketController(accountManager.getLoggedInUser()));
         GridPane.setConstraints(userPane, 0, 0);
         center.getChildren().clear();
         center.getChildren().addAll(userPane);
@@ -64,7 +64,7 @@ public class SideMenuController extends AbstractController implements Initializa
     }
 
     private void wishList() {
-        Parent userPane = loadPane("add_wish.fxml", new UserInfoPresenter(accountManager.getLoggedInUser()));
+        Parent userPane = loadPane("add_wish.fxml", new WishItemAddController());
         GridPane.setConstraints(userPane, 0, 0);
         center.getChildren().clear();
         center.getChildren().addAll(userPane);

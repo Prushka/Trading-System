b15a103ffb495cdde627b1a939d6776c296056d7
Grace Leung <greyse.leung@mail.utoronto.ca>
2020-08-16 22:49:47 -0400

    fixing errors caused by userOrderBundle making

--- phase2/src/main/java/phase2/trade/trade/TradeCreator.java
+++ phase2/src/main/java/phase2/trade/trade/TradeCreator.java
@@ -15,9 +15,9 @@ public class TradeCreator {
     public Trade createTrade(List<User> users, List<Set<Item>> items, String year, String month, String day, String hour,
                              String minute, String country, String city, String isPermanent){
         tb = new TradeBuilder();
-//        for (int i = 0; i < users.size(); i++){
-//            tb.buildUserOrderBundle(users.get(i), items.get(i));
-//        }
+        for (int i = 0; i < users.size(); i++){
+            tb.buildUserOrderBundle(users.get(i), items.get(i));
+        }
         tb.buildDateTime(year, month, day, hour, minute);
         tb.buildLocation(country, city);
         tb.buildIsPermanent(Boolean.parseBoolean(isPermanent));
